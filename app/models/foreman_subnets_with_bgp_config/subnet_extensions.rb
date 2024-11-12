b83cdd103aec44bf7e081805971227009eff658e
Xavier Mol <xavier.mol@kit.edu>
2024-11-12 11:40:18 +0100

    need to class_eval a block to define associations

--- app/models/foreman_subnets_with_bgp_config/subnet_extensions.rb
+++ app/models/foreman_subnets_with_bgp_config/subnet_extensions.rb
@@ -1,12 +1,14 @@
 module ForemanSubnetsWithBGPConfig
   module SubnetExtensions
     def self.included (base)
-      has_one :subnet_bgp_config,
-        inverse_of: :subnet,
-        dependent: :destroy
-      accepts_nested_attributes_for :subnet_bgp_config,
-        reject_if: :all_blank,
-        update_only: true
+      base.class_eval do
+        has_one :subnet_bgp_config,
+          inverse_of: :subnet,
+          dependent: :destroy
+        accepts_nested_attributes_for :subnet_bgp_config,
+          reject_if: :all_blank,
+          update_only: true
+      end
     end
   end
 end
